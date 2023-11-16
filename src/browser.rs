102c1cd415e8315a4db387b586f5cfe2e79b9639
vaquero <froilan@virtual-artifact.com>
2023-11-16 23:55:52 +0100

    add anyhow crate to project, refactor window and document into browser mod

--- src/browser.rs
+++ src/browser.rs
@@ -1,6 +1,19 @@
+use anyhow::{anyhow, Result};
+use web_sys::{Document, Window};
+
 macro_rules! log {
     ( $( $t:tt )* ) => {
         web_sys::console::log_1(&format!( $( $t )*
                 ).into());
     }
+}
+
+pub fn window() -> Result<Window> {
+    web_sys::window().ok_or_else(|| anyhow!("Window not found"))
+}
+
+pub fn document() -> Result<Document> {
+    window()?
+        .document()
+        .ok_or_else(|| anyhow!("Document not found"))
 }
