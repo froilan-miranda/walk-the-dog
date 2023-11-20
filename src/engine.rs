91e966081b19d60d97aae3e523deab8ae29217dd
vaquero <froilan@virtual-artifact.com>
2023-11-20 13:23:58 +0100

    fix the time step

--- src/engine.rs
+++ src/engine.rs
@@ -1,12 +1,12 @@
+use crate::browser::LoopClosure;
 use anyhow::{anyhow, Result};
 use futures::channel::oneshot::channel;
+use std::cell::RefCell;
 use std::rc::Rc;
 use std::sync::Mutex;
 use wasm_bindgen::prelude::*;
-use web_sys::HtmlImageElement;
 use web_sys::CanvasRenderingContext2d;
-use crate::browser::LoopClosure;
-use std::cell::RefCell;
+use web_sys::HtmlImageElement;
 
 use crate::browser;
 
@@ -15,14 +15,28 @@ pub trait Game {
     fn draw(&self, context: &CanvasRenderingContext2d);
 }
 
-pub struct GameLoop;
+const FRAME_SIZE: f32 = 1.0 / 60.0 * 1000.0;
+pub struct GameLoop {
+    last_frame: f64,
+    accumulated_delta: f32,
+}
+
 type SharedLoopClosure = Rc<RefCell<Option<LoopClosure>>>;
 impl GameLoop {
     pub async fn start(mut game: impl Game + 'static) -> Result<()> {
+        let mut game_loop = GameLoop {
+            last_frame: browser::now()?,
+            accumulated_delta: 0.0,
+        };
         let f: SharedLoopClosure = Rc::new(RefCell::new(None));
         let g = f.clone();
         *g.borrow_mut() = Some(browser::create_raf_closure(move |perf: f64| {
-            game.update();
+            game_loop.accumulated_delta += (perf - game_loop.last_frame) as f32;
+            while game_loop.accumulated_delta > FRAME_SIZE {
+                game.update();
+                game_loop.accumulated_delta -= FRAME_SIZE;
+            }
+            game_loop.last_frame = perf;
             game.draw(&browser::context().expect("Context should exist"));
             browser::request_animation_frame(f.borrow().as_ref().unwrap());
         }));
