e54ebc98b196ce01d52ae45217b9106e2ce66520
vaquero <froilan@virtual-artifact.com>
2023-12-03 19:05:33 +0100

    reimplement drawing

--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,5 @@
-use anyhow::Result;
 use anyhow::anyhow;
+use anyhow::Result;
 use async_trait::async_trait;
 use serde::Deserialize;
 use std::collections::HashMap;
@@ -39,12 +39,38 @@ struct RedHatBoy {
 impl RedHatBoy {
     fn new(sheet: Sheet, image: HtmlImageElement) -> Self {
         RedHatBoy {
-            state_machine: RedHatBoyStateMachine::Idle(
-                               RedHatBoyState::new()
-                           ),
+            state_machine: RedHatBoyStateMachine::Idle(RedHatBoyState::new()),
             sprite_sheet: sheet,
             image,
         }
+    }
+
+    fn draw(&self, renderer: &Renderer) {
+        let frame_name = format!(
+            "{} ({}).png",
+            self.state_machine.frame_name(),
+            (self.state_machine.context().frame / 3) + 1
+        );
+        let sprite = self
+            .sprite_sheet
+            .frames
+            .get(&frame_name)
+            .expect("Cell not found");
+        renderer.draw_image(
+            &self.image,
+            &Rect {
+                x: sprite.frame.x.into(),
+                y: sprite.frame.y.into(),
+                width: sprite.frame.w.into(),
+                height: sprite.frame.h.into(),
+            },
+            &Rect {
+                x: self.state_machine.context().position.x.into(),
+                y: self.state_machine.context().position.y.into(),
+                width: sprite.frame.w.into(),
+                height: sprite.frame.h.into(),
+            },
+        );
     }
 }
 
@@ -61,9 +87,20 @@ pub enum Event {
 impl RedHatBoyStateMachine {
     fn transition(self, event: Event) -> Self {
         match (self, event) {
-            (RedHatBoyStateMachine::Idle(state),
-             Event::Run) => state.run().into(),
+            (RedHatBoyStateMachine::Idle(state), Event::Run) => state.run().into(),
             _ => self,
+        }
+    }
+    fn frame_name(&self) -> &str {
+        match self {
+            RedHatBoyStateMachine::Idle(state) => state.frame_name(),
+            RedHatBoyStateMachine::Running(state) => state.frame_name(),
+        }
+    }
+    fn context(&self) -> &RedHatBoyContext {
+        match self {
+            RedHatBoyStateMachine::Idle(state) => &state.context(),
+            RedHatBoyStateMachine::Running(state) => &state.context(),
         }
     }
 }
@@ -76,16 +113,23 @@ impl From<RedHatBoyState<Running>> for RedHatBoyStateMachine {
 mod red_hat_boy_states {
     use crate::engine::Point;
     const FLOOR: i16 = 475;
+    const IDLE_FRAME_NAME: &str = "Idle";
+    const RUN_FRAME_NAME: &str = "Run";
     #[derive(Copy, Clone)]
     pub struct RedHatBoyState<S> {
         context: RedHatBoyContext,
         _state: S,
     }
+    impl<S> RedHatBoyState<S> {
+        pub fn context(&self) -> &RedHatBoyContext {
+            &self.context
+        }
+    }
     #[derive(Copy, Clone)]
     pub struct RedHatBoyContext {
-        frame: u8,
-        position: Point,
-        velocity: Point,
+        pub frame: u8,
+        pub position: Point,
+        pub velocity: Point,
     }
     #[derive(Copy, Clone)]
     pub struct Idle;
@@ -103,14 +147,20 @@ mod red_hat_boy_states {
                 _state: Idle {},
             }
         }
-    }
-
-    impl RedHatBoyState<Idle> {
         pub fn run(self) -> RedHatBoyState<Running> {
             RedHatBoyState {
                 context: self.context,
                 _state: Running {},
             }
+        }
+        pub fn frame_name(&self) -> &str {
+            IDLE_FRAME_NAME
+        }
+    }
+
+    impl RedHatBoyState<Running> {
+        pub fn frame_name(&self) -> &str {
+            RUN_FRAME_NAME
         }
     }
 }
@@ -144,8 +194,8 @@ impl Game for WalkTheDog {
             frame: self.frame,
             position: self.position,
             rhb: Some(RedHatBoy::new(
-                    sheet.clone().ok_or_else(|| anyhow! ("No Sheet Present"))?,
-                    image.ok_or_else(|| anyhow! ("No Image Present"))?,
+                sheet.clone().ok_or_else(|| anyhow!("No Sheet Present"))?,
+                image.ok_or_else(|| anyhow!("No Image Present"))?,
             )),
         }))
     }
@@ -202,5 +252,6 @@ impl Game for WalkTheDog {
                 },
             );
         });
+        self.rhb.as_ref().unwrap().draw(renderer);
     }
 }
